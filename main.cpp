151139f996beb38f437e4a4f37bee89bfcc734f9
owencalvin <owen.gombas@gmail.com>
2022-04-10 17:01:07 +0200

    return weight of paths for prim and dijkstra

--- main.cpp
+++ main.cpp
@@ -6,53 +6,8 @@
 
 using namespace std;
 
-void printBasicGraph() {
-    const Graph g = Graphs::getBasicGraph();
-
-    cout << "First graph g:";
-    cout << g;
-
-    cout << endl << "Raw matrix: " << endl;
-    cout << g.rawMatrixString() << endl;
-
-    cout << "Recursive depth first: ";
-    g.recursiveDepthFirstSearch();
-
-    cout << endl << "Iterative depth first: ";
-    g.iterativeDepthFirstSearch();
-
-    cout << endl << "Iterative breadth first: ";
-    g.iterativeBreadthFirstSearch();
-
-    cout << endl << "Iterative priority first (=== depth first): ";
-    g.iterativePriorityFirstSearch(-1);
-
-    cout << endl << "Iterative priority first (=== breadth first): ";
-    g.iterativePriorityFirstSearch(1);
-
-    cout << endl << endl << endl;
-}
-
-void printPrimGraph() {
-    const Graph g = Graphs::getPrimGraph();
-
-    cout << "Second graph g:";
-    cout << g;
-
-    cout << endl << endl << "Prim: " << endl;
-    g.prim(true);
-
-    cout << endl << "Dijkstra: " << endl;
-    g.dijkstra(true);
-
-    cout << endl << endl << endl;
-}
-
-void printFileGraphs() {
-    Graph g = Graph("graphs/graph1.txt");
-
-    cout << "First graph g:";
-    cout << g << endl;
+void printGraph(const Graph& g) {
+    cout << g << endl << endl;
 
     cout << "Recursive depth first: ";
     g.recursiveDepthFirstSearch();
@@ -73,17 +28,16 @@ void printFileGraphs() {
     int primWeight = g.prim(true);
     cout << "Prim total weight: " << primWeight << endl;
 
-    cout << endl << endl << "Dijkstra: " << endl;
+    cout << endl << "Dijkstra: " << endl;
     int dijkstraWeight = g.dijkstra(true);
     cout << "Dijkstra total weight: " << dijkstraWeight << endl;
 
-    cout << endl << endl << endl;
+    cout << endl << endl;
 }
 
 int main() {
-    printBasicGraph();
-    printPrimGraph();
-    printFileGraphs();
+    Graph g = Graph("graphs/graph1.txt");
+    printGraph(g);
 
     return 0;
 }
