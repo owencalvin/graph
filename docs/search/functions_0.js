f6baa50f09d589189aa4b740fc87c8fd6a18296c
owencalvin <owen.gombas@gmail.com>
2022-04-10 14:38:55 +0200

    fix log

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['addarc_0',['addArc',['../class_graph.html#a56824a4a2a4ce54bab948c7f23755de3',1,'Graph']]],
   ['addarcs_1',['addArcs',['../class_graph.html#a34c9ab7622b0a617d6b4c99ac4d226a7',1,'Graph']]],
-  ['addorientedgraph_2',['addOrientedGraph',['../class_graph.html#af8fa809db0138b0016b9f92886299717',1,'Graph']]]
+  ['addorientedarc_2',['addOrientedArc',['../class_graph.html#a6e4abfec858ce79e2e79a0bc8d50e6c5',1,'Graph']]]
 ];
